0f79907a48501cda37f00548db6a9e56a3b9d317
anuragnibhamishra <theanuragmishra19@gmail.com>
2026-06-02 23:57:41 +0530

    HomePageFrontEnd

--- client/src/components/Home/AppPromoBanner.tsx
+++ client/src/components/Home/AppPromoBanner.tsx
@@ -1,8 +1,19 @@
+import { appPromoBannerData, assets } from "../../assets/assets"
 
 const AppPromoBanner = () => {
   return (
-    <section>
-        
+    <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 my-14 bg-green-950 rounded-2xl">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 xl:px-10 ">
+        <div className="text-center md:text-left">
+          <h2 className="text-3xl sm:text-4xl text-white mb-3 font-serif">{appPromoBannerData.title}</h2>
+          <p className="text-white/70 mb-6 max-w-md">{appPromoBannerData.description}</p>
+          <div className="flex flex-wrap gap-3 justify-center md:justify-start">
+            <button className="px-6 py-3 font-semibold rounded-xl bg-white text-green-950 hover:bg-orange-100"> App Store </button>
+            <button className="px-6 py-3 font-semibold rounded-xl bg-white/10 text-white hover:bg-white/20 transition-colors border border-white/20"> Google Play </button>
+          </div>
+        </div>
+        <img src={assets.delivery_truck} alt="delivery-truck" className="max-w-60 sm:max-w-120 xl:pr-10 " />
+      </div>
     </section>
   )
 }
