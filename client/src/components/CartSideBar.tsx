e5c1a0fcd5d064cafe1225d770253e069333b7c0
anuragnibhamishra <theanuragmishra19@gmail.com>
2026-06-04 23:56:30 +0530

    CartContext

--- client/src/components/CartSideBar.tsx
+++ client/src/components/CartSideBar.tsx
@@ -1,9 +1,12 @@
 
 const CartSideBar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="fixed inset-0 bg-black/40 z-50 transition-opacity" />
+      <div className="fixed right-0 top-0 h-full w-full max-w-md bg-white z-50 shadow-2xl flex flex-col animate-slide-in-right ">
+
+      </div>
+    </>
   )
 }
 
